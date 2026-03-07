b8ed6046376e008cf612203e9335b4b0b7041d0d
B KORNEL <boyalapallikornelu36@gmail.com>
2026-03-07 10:19:09 +0530

    Add files via upload

---
+++ 01_python_basics/4_condition_statements/a)_if_else.py
@@ -0,0 +1,37 @@
+#==========================================================================
+#                            if-else
+#==========================================================================
+
+
+# 1.ATM Withdrawal Check
+# -------------------------------------------------------------------
+total_amount = float(input("Enter total amount:"))
+with_draw_amount = int(input("Enter withdraw amount:"))
+
+if with_draw_amount <= total_amount:
+    print("Withdrawa is succesfull")
+else:
+    print("Insufficent Balance")
+# ---------------------------------------------------------------------
+
+
+
+# 2.Website Age Restriction
+# ---------------------------------------------------------------------
+user_age = int(input("Enter your age:"))
+if user_age >= 18:
+    print("Access granted")
+else:
+    print("Access Denied Age Restricted Content")
+# ----------------------------------------------------------------------
+
+
+
+# 3.Even or Odd Order ID
+# ----------------------------------------------------------------------
+order_id = int(input("Enter your Order Id : "))
+if order_id % 2 == 0:
+    print("Priority processing")
+else:
+    print("Normal praocessing")
+# ----------------------------------------------------------------------
